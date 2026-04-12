7b1d248c8eec0abe544d228ce2775744e57fc512
Ziad Tamer <ztamer494@gmail.com>
2026-04-12 17:31:15 +0200

    create splash view

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:ai_eru_tawasol/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const AiEruTawasol());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);

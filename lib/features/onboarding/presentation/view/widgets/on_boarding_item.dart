6153f559c193cb5eba83f1ba699b367127bf279a
Ziad Tamer <ztamer494@gmail.com>
2026-04-13 19:30:03 +0200

    implement OnBoardingItem widget for onboarding flow

---
+++ lib/features/onboarding/presentation/view/widgets/on_boarding_item.dart
@@ -0,0 +1,31 @@
+import 'package:ai_eru_tawasol/features/onboarding/data/model/on_boarding_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class OnBoardingItem extends StatelessWidget {
+  const OnBoardingItem({super.key, required this.model});
+
+  final OnBoardingModel model;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 50),
+        SvgPicture.asset(model.image),
+        const SizedBox(height: 20),
+        Text(
+          model.title,
+          textAlign: TextAlign.center,
+          style: const TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+        ),
+        const SizedBox(height: 13),
+        Text(
+          model.subtitle,
+          textAlign: TextAlign.center,
+          style: const TextStyle(fontSize: 16),
+        ),
+      ],
+    );
+  }
+}

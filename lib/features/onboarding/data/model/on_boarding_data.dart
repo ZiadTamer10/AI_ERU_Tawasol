fc114802df11e924f6f6031d564bfe0418990867
Ziad Tamer <ztamer494@gmail.com>
2026-04-13 19:23:13 +0200

    create on boarding list

---
+++ lib/features/onboarding/data/model/on_boarding_data.dart
@@ -0,0 +1,33 @@
+import 'package:ai_eru_tawasol/core/utils/assets.dart';
+import 'package:ai_eru_tawasol/features/onboarding/data/model/on_boarding_model.dart';
+
+final List<OnBoardingModel> onBoardingList = [
+  OnBoardingModel(
+    image: AssetsData.onBoarding1,
+    title: 'Welcome to Cybex IT Group\nwhere learning meets innovation!',
+    subtitle: 'Empowering your journey through\ncutting-edge IT education',
+  ),
+  OnBoardingModel(
+    image: AssetsData.onBoarding1,
+    title: 'Begin your learning journey and \nunlock a world of knowledge',
+    subtitle:
+        'Explore our comprehensive courses \ndesigned to transform your skills and \ncareer',
+  ),
+  OnBoardingModel(
+    image: AssetsData.onBoarding1,
+    title: 'Dive into a seamless learning \nexperience with Cybex IT Group',
+    subtitle:
+        'Experience interactive learning with\n expert-led courses and progress \ntracking',
+  ),
+  OnBoardingModel(
+    image: AssetsData.onBoarding1,
+    title: 'Join a community of learners and\n embark on a learning adventure',
+    subtitle:
+        'Connect with like-minded individuals \nJoin us to learn, grow, and thrive \ntogether!',
+  ),
+  OnBoardingModel(
+    image: AssetsData.onBoarding1,
+    title: 'Join Cybex IT Group to Kick Start\n Your Lesson',
+    subtitle: 'Join and Learn from our Top Instructors!',
+  ),
+];

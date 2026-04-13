2dcd5202c09307faf577c27560f63ced33d30e2f
Ziad Tamer <ztamer494@gmail.com>
2026-04-13 17:21:12 +0200

    create on boarding 1 view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,12 +1,18 @@
 import 'package:ai_eru_tawasol/features/authentication/presentation/view/login_view.dart';
+import 'package:ai_eru_tawasol/features/onboarding/presentation/view/on_boarding1_view.dart';
 import 'package:ai_eru_tawasol/features/splash/presentation/view/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouter {
   static const kLoginView = '/loginView';
+  static const kOnBoardingView1 = '/onBoardingView1';
   static final GoRouter router = GoRouter(
     routes: [
       GoRoute(path: '/', builder: (context, state) => SplashView()),
+      GoRoute(
+        path: kOnBoardingView1,
+        builder: (context, state) => OnBoarding1View(),
+      ),
       GoRoute(path: kLoginView, builder: (context, state) => LoginView()),
     ],
   );

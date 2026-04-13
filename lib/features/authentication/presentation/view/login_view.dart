e3e7ac310a8e4ad46c95262500ebc0b1abda67bd
Ziad Tamer <ztamer494@gmail.com>
2026-04-13 17:10:26 +0200

    create login view

--- lib/features/authentication/presentation/view/login_view.dart
+++ lib/features/authentication/presentation/view/login_view.dart
@@ -1,3 +1,4 @@
+import 'package:ai_eru_tawasol/features/authentication/presentation/view/widgets/login_view_body.dart';
 import 'package:flutter/material.dart';
 
 class LoginView extends StatelessWidget {
@@ -5,6 +6,6 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(body: LoginViewBody());
   }
 }

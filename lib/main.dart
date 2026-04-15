e0ff62f786ca3c8c46db53dadf383c191994f4ce
Ziad Tamer <ztamer494@gmail.com>
2026-04-15 20:27:38 +0200

    add font to our app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:ai_eru_tawasol/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const AiEruTawasol());
@@ -12,6 +13,7 @@ class AiEruTawasol extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      theme: ThemeData(textTheme: GoogleFonts.plusJakartaSansTextTheme()),
       debugShowCheckedModeBanner: false,
       routerConfig: AppRouter.router,
     );

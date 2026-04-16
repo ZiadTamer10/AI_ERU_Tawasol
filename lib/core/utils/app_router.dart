eb889cd4e7a13722794cbc7e86b0c6e348e42814
Ziad Tamer <ztamer494@gmail.com>
2026-04-16 23:45:25 +0200

    provie cubit to forget password view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:ai_eru_tawasol/core/utils/service_locator.dart';
 import 'package:ai_eru_tawasol/features/authentication/data/repos/auth_repo_impl.dart';
+import 'package:ai_eru_tawasol/features/authentication/presentation/manager/forget_password_cubit/forget_password_cubit.dart';
 import 'package:ai_eru_tawasol/features/authentication/presentation/manager/login_cubit/login_cubit.dart';
 import 'package:ai_eru_tawasol/features/authentication/presentation/view/forget_password_view.dart';
 import 'package:ai_eru_tawasol/features/authentication/presentation/view/login_view.dart';
@@ -30,7 +31,10 @@ class AppRouter {
       ),
       GoRoute(
         path: kForgetPasswordView,
-        builder: (context, state) => ForgetPasswordView(),
+        builder: (context, state) => BlocProvider(
+          create: (context) => ForgetPasswordCubit(getIt.get<AuthRepoImpl>()),
+          child: ForgetPasswordView(),
+        ),
       ),
       GoRoute(
         path: kDoctorHomeView,

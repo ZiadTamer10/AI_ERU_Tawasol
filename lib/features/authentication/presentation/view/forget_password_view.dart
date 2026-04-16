1c475ff462bd5b07100a8323a0572bd6e7a8941f
Ziad Tamer <ztamer494@gmail.com>
2026-04-16 22:50:04 +0200

    create forget password view

---
+++ lib/features/authentication/presentation/view/forget_password_view.dart
@@ -0,0 +1,11 @@
+import 'package:ai_eru_tawasol/features/authentication/presentation/view/widgets/forget_password_view_body.dart';
+import 'package:flutter/material.dart';
+
+class ForgetPasswordView extends StatelessWidget {
+  const ForgetPasswordView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: ForgetPasswordViewBody());
+  }
+}

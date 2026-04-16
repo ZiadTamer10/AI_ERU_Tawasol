d52af3e1cb88bd52a5bf6617e16e826893553684
Ziad Tamer <ztamer494@gmail.com>
2026-04-17 00:16:37 +0200

    reset password done

--- lib/features/authentication/presentation/view/widgets/forget_password_view_body.dart
+++ lib/features/authentication/presentation/view/widgets/forget_password_view_body.dart
@@ -1,6 +1,8 @@
+import 'package:ai_eru_tawasol/features/authentication/presentation/manager/forget_password_cubit/forget_password_cubit.dart';
 import 'package:ai_eru_tawasol/features/authentication/presentation/view/widgets/custom_button.dart';
 import 'package:ai_eru_tawasol/features/authentication/presentation/view/widgets/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ForgetPasswordViewBody extends StatefulWidget {
   const ForgetPasswordViewBody({super.key});
@@ -10,45 +12,81 @@ class ForgetPasswordViewBody extends StatefulWidget {
 }
 
 class _ForgetPasswordViewBodyState extends State<ForgetPasswordViewBody> {
+  final TextEditingController emailController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    super.dispose();
+  }
+
+  void sendResetLink() {
+    if (formKey.currentState!.validate()) {
+      context.read<ForgetPasswordCubit>().forgetPassword(
+        emailController.text.trim(),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    final emailController = TextEditingController();
-    final formKey = GlobalKey<FormState>();
+    return BlocConsumer<ForgetPasswordCubit, ForgetPasswordState>(
+      listener: (context, state) {
+        if (state is ForgetPasswordSuccess) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text("Reset email sent successfully")),
+          );
+        }
 
-    @override
-    void dispose() {
-      emailController.dispose();
-      super.dispose();
-    }
+        if (state is ForgetPasswordFailure) {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text(state.errMessage)));
+        }
+      },
+      builder: (context, state) {
+        return Form(
+          key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 80),
 
-    return Form(
-      key: formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SizedBox(height: 80),
-          const Text(
-            "Forgot Password",
-            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              const Text(
+                "Forgot Password",
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+
+              const SizedBox(height: 20),
+
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Text(
+                  "Enter your university email and we will send you a link to reset your password.",
+                  style: TextStyle(color: Colors.grey, fontSize: 16),
+                ),
+              ),
+
+              const SizedBox(height: 30),
+
+              CustomTextFormField(
+                hintText: 'Email',
+                controller: emailController,
+              ),
+
+              const SizedBox(height: 30),
+
+              state is ForgetPasswordLoading
+                  ? const CircularProgressIndicator()
+                  : CustomButton(
+                      text: 'Send Reset Link',
+                      onPressed: sendResetLink,
+                    ),
+            ],
           ),
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: const Text(
-              "Enter your university email and we will send you a link to reset your password.",
-              style: TextStyle(color: Colors.grey, fontSize: 16),
-            ),
-          ),
-          const SizedBox(height: 30),
-          CustomTextFormField(hintText: 'Email'),
-          const SizedBox(height: 30),
-          SizedBox(
-            width: double.infinity,
-            height: 60,
-            child: CustomButton(text: 'Send Reset Link', onPressed: () {}),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }

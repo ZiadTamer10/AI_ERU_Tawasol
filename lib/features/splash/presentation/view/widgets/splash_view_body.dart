63867b4e1486a24d22f4890a6e6865d5e93f7a64
Ziad Tamer <ztamer494@gmail.com>
2026-04-16 04:29:33 +0200

    splash view done

--- lib/features/splash/presentation/view/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/view/widgets/splash_view_body.dart
@@ -16,12 +16,78 @@ class _SplashViewBodyState extends State<SplashViewBody> {
     Future.delayed(const Duration(seconds: 2), () {
       GoRouter.of(context).pushReplacement(AppRouter.kOnBoardingView1);
     });
-
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Image.asset(AssetsData.logo));
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+        gradient: RadialGradient(
+          center: Alignment.center,
+          radius: 0.8,
+          colors: [
+            Color(0xFF66B2FF),
+            Color(0xFF2E7BFF),
+            Color(0xFF0B3D91),
+            Color(0xFF001220),
+          ],
+          stops: [0.0, 0.3, 0.6, 1.0],
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(AssetsData.logo2),
+          Stack(
+            children: [
+              Text(
+                'AI Connect',
+                style: TextStyle(
+                  fontSize: 47,
+                  fontWeight: FontWeight.bold,
+                  foreground: Paint()
+                    ..style = PaintingStyle.stroke
+                    ..strokeWidth = 6
+                    ..color = Colors.black,
+                  shadows: const [
+                    Shadow(
+                      offset: Offset(4, 4),
+                      blurRadius: 10,
+                      color: Colors.black54,
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                'AI Connect',
+                style: TextStyle(
+                  fontSize: 46,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFFFDE992),
+                  shadows: const [
+                    Shadow(
+                      offset: Offset(4, 4),
+                      blurRadius: 10,
+                      color: Colors.black54,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Text(
+            'EGYPTIAN RUSSIAN UNIVERSITY',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 12,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

53cea192ace74ca51147e2b2120d0b3329058e05
Ziad Tamer <ztamer494@gmail.com>
2026-04-16 03:23:27 +0200

    create service locator

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,12 @@
+import 'package:ai_eru_tawasol/core/utils/auth_services.dart';
+import 'package:ai_eru_tawasol/features/authentication/data/repos/auth_repo_impl.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setupServiceLocator() {
+  getIt.registerSingleton<AuthServices>(AuthServices());
+  getIt.registerSingleton<AuthRepoImpl>(
+    AuthRepoImpl(getIt.get<AuthServices>()),
+  );
+}

9f8e64ab2afff2ad4d1adf2d0adf69c20b1a7d26
Ziad Tamer <ztamer494@gmail.com>
2026-04-16 03:26:32 +0200

    provide cubit to login view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,6 +1,10 @@
+import 'package:ai_eru_tawasol/core/utils/service_locator.dart';
+import 'package:ai_eru_tawasol/features/authentication/data/repos/auth_repo_impl.dart';
+import 'package:ai_eru_tawasol/features/authentication/presentation/manager/login_cubit/login_cubit.dart';
 import 'package:ai_eru_tawasol/features/authentication/presentation/view/login_view.dart';
 import 'package:ai_eru_tawasol/features/onboarding/presentation/view/on_boarding_view.dart';
 import 'package:ai_eru_tawasol/features/splash/presentation/view/splash_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouter {
@@ -13,7 +17,13 @@ class AppRouter {
         path: kOnBoardingView1,
         builder: (context, state) => OnBoardingView(),
       ),
-      GoRoute(path: kLoginView, builder: (context, state) => LoginView()),
+      GoRoute(
+        path: kLoginView,
+        builder: (context, state) => BlocProvider(
+          create: (context) => LoginCubit(getIt.get<AuthRepoImpl>()),
+          child: LoginView(),
+        ),
+      ),
     ],
   );
 }

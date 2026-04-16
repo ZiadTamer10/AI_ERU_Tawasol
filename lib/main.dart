5a4075e4f68b001f016aad6e2ee426a32400b0e9
Ziad Tamer <ztamer494@gmail.com>
2026-04-16 02:53:34 +0200

    add firebase to our project

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:ai_eru_tawasol/core/utils/app_router.dart';
+import 'package:ai_eru_tawasol/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const AiEruTawasol());
 }
 
 class AiEruTawasol extends StatelessWidget {
   const AiEruTawasol({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(

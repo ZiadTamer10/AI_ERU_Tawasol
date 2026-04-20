65a055b5e2d47b588b7e587b7072f98c1cf93f85
Ziad Tamer <ztamer494@gmail.com>
2026-04-20 09:46:22 +0200

    refactor bottom navigation bar

---
+++ lib/features/home/presentation/view/widgets/home_bottom_navigation_bar.dart
@@ -0,0 +1,44 @@
+import 'package:ai_eru_tawasol/constants.dart';
+import 'package:flutter/material.dart';
+
+class HomeBottomNavBar extends StatelessWidget {
+  const HomeBottomNavBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  final int currentIndex;
+  final Function(int) onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      type: BottomNavigationBarType.shifting,
+      selectedItemColor: kPrimaryColor,
+      unselectedItemColor: Colors.grey,
+      elevation: 0.01,
+      iconSize: 32,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.campaign_outlined),
+          label: "Announcements",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.menu_book_outlined),
+          label: "Courses",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_month_outlined),
+          label: "Calendar",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
+          label: "Profile",
+        ),
+      ],
+    );
+  }
+}
--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -1,4 +1,4 @@
-import 'package:ai_eru_tawasol/constants.dart';
+import 'package:ai_eru_tawasol/features/home/presentation/view/widgets/home_bottom_navigation_bar.dart';
 import 'package:ai_eru_tawasol/features/home/presentation/view/widgets/pages.dart';
 import 'package:flutter/material.dart';
 
@@ -22,26 +22,9 @@ class _HomeViewBodyState extends State<HomeViewBody> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: pages[currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
+      bottomNavigationBar: HomeBottomNavBar(
         currentIndex: currentIndex,
         onTap: onItemTapped,
-        type: BottomNavigationBarType.shifting,
-        selectedItemColor: kPrimaryColor,
-        unselectedItemColor: Colors.grey,
-        elevation: 0.01,
-        iconSize: 32,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.menu_book),
-            label: "Courses",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_month),
-            label: "Calendar",
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
-        ],
       ),
     );
   }

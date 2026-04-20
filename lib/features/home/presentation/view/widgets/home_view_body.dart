3ded84a28a0b557dcbd93944687bd6dc49d5cccf
Ziad Tamer <ztamer494@gmail.com>
2026-04-20 09:35:16 +0200

    add bottom navigation bar to home view body

--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -1,10 +1,55 @@
+import 'package:ai_eru_tawasol/constants.dart';
 import 'package:flutter/material.dart';
 
-class HomeViewBody extends StatelessWidget {
+class HomeViewBody extends StatefulWidget {
   const HomeViewBody({super.key});
 
   @override
+  State<HomeViewBody> createState() => _HomeViewBodyState();
+}
+
+class _HomeViewBodyState extends State<HomeViewBody> {
+  int currentIndex = 0;
+
+  final List<Widget> pages = const [
+    Center(child: Text("Announcements")),
+    Center(child: Text("Courses")),
+    Center(child: Text("Calendar")),
+    Center(child: Text("Profile")),
+  ];
+
+  void onItemTapped(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: pages[currentIndex],
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: onItemTapped,
+        type: BottomNavigationBarType.shifting,
+        selectedItemColor: kPrimaryColor,
+        unselectedItemColor: Colors.grey,
+        elevation: 0.01,
+        iconSize: 32,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu_book),
+            label: "Courses",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: "Calendar",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+      ),
+    );
   }
 }

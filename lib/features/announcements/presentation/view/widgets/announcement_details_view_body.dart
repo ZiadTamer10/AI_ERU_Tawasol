53db62c1f3639bb3f03cd09f25d85866a9d823ce
Ziad Tamer <ztamer494@gmail.com>
2026-04-22 15:48:44 +0200

    add custom announcement details app bar to announcement details view

--- lib/features/announcements/presentation/view/widgets/announcement_details_view_body.dart
+++ lib/features/announcements/presentation/view/widgets/announcement_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:ai_eru_tawasol/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class AnnouncementDetailsViewBody extends StatelessWidget {
@@ -5,6 +6,18 @@ class AnnouncementDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Column(children: [CustomAnnouncementDetailsAppBar()]);
+  }
+}
+
+class CustomAnnouncementDetailsAppBar extends StatelessWidget {
+  const CustomAnnouncementDetailsAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text('Announcement Details', style: Styles.textStyle22),
+      centerTitle: true,
+    );
   }
 }

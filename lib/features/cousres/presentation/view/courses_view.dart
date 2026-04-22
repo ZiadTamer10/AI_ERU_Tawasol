763be85024fa11cf83efb142a2441b57589faeaf
Ziad Tamer <ztamer494@gmail.com>
2026-04-22 13:45:46 +0200

    add courses view to home view body

---
+++ lib/features/cousres/presentation/view/courses_view.dart
@@ -0,0 +1,11 @@
+import 'package:ai_eru_tawasol/features/cousres/presentation/view/widgets/courser_view_body.dart';
+import 'package:flutter/material.dart';
+
+class CoursesView extends StatelessWidget {
+  const CoursesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: CourserViewBody());
+  }
+}
--- lib/features/home/presentation/view/widgets/pages.dart
+++ lib/features/home/presentation/view/widgets/pages.dart
@@ -1,9 +1,10 @@
 import 'package:ai_eru_tawasol/features/announcements/presentation/view/announcements_view.dart';
+import 'package:ai_eru_tawasol/features/cousres/presentation/view/widgets/courser_view_body.dart';
 import 'package:flutter/material.dart';
 
 final List<Widget> pages = const [
   AnnouncementsView(),
-  Center(child: Text("Courses")),
+  CourserViewBody(),
   Center(child: Text("Calendar")),
   Center(child: Text("Profile")),
 ];

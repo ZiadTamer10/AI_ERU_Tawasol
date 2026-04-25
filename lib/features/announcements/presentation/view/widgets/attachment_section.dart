7c82137b7aa8fb06b654822cec5ee4496ad55a7a
Ziad Tamer <ztamer494@gmail.com>
2026-04-25 04:34:43 +0300

    refactor attachment section and add attachment card widget

--- lib/features/announcements/presentation/view/widgets/attachment_section.dart
+++ lib/features/announcements/presentation/view/widgets/attachment_section.dart
@@ -1,7 +1,6 @@
-import 'package:ai_eru_tawasol/core/utils/assets.dart';
 import 'package:ai_eru_tawasol/core/utils/styles.dart';
+import 'package:ai_eru_tawasol/features/announcements/presentation/view/widgets/attachment_card.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class AttachmentSection extends StatelessWidget {
   const AttachmentSection({super.key});
@@ -15,35 +14,7 @@ class AttachmentSection extends StatelessWidget {
         children: [
           Text('Attachments', style: Styles.textStyle22),
           SizedBox(height: 10),
-          Container(
-            height: 80,
-            decoration: BoxDecoration(
-              color: Color(0xffF9F9F9),
-              border: Border.all(color: Color(0xffC4C4C4)),
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 8),
-                  child: SvgPicture.asset(AssetsData.pdfIcon, height: 60),
-                ),
-                Spacer(),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('lecture4.pdf', style: Styles.textStyle18),
-                    Text('2.4 MB', style: Styles.textStyle18),
-                  ],
-                ),
-                Spacer(flex: 9),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.file_download_outlined, size: 32),
-                ),
-              ],
-            ),
-          ),
+          AttachmentCard(),
         ],
       ),
     );

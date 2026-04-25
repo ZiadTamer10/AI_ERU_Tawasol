5cc966eece0aa1e6b4325a83afe595790406b918
Ziad Tamer <ztamer494@gmail.com>
2026-04-25 16:02:05 +0300

    add courses list view to course view body

--- lib/features/cousres/presentation/view/widgets/courses_view_body.dart
+++ lib/features/cousres/presentation/view/widgets/courses_view_body.dart
@@ -1,4 +1,4 @@
-import 'package:ai_eru_tawasol/features/cousres/presentation/view/widgets/course_card.dart';
+import 'package:ai_eru_tawasol/features/cousres/presentation/view/widgets/courses_list_view.dart';
 import 'package:ai_eru_tawasol/features/cousres/presentation/view/widgets/custom_courses_app_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -9,18 +9,15 @@ class CoursesViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
+      body: ListView(
         physics: const BouncingScrollPhysics(),
-        child: const Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CustomCoursesAppBar(),
-            SizedBox(height: 10),
-            Divider(color: Color(0xffC4C4C4), thickness: 2),
-            SizedBox(height: 30),
-            CourseCard(),
-          ],
-        ),
+        children: const [
+          CustomCoursesAppBar(),
+          SizedBox(height: 10),
+          Divider(color: Color(0xffC4C4C4), thickness: 2),
+          SizedBox(height: 30),
+          CoursesListView(),
+        ],
       ),
     );
   }

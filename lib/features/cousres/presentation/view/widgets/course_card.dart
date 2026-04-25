49e34ece21c209886b14eb80d320e696ed53835f
Ziad Tamer <ztamer494@gmail.com>
2026-04-25 10:16:24 +0300

    refactor course card

--- lib/features/cousres/presentation/view/widgets/course_card.dart
+++ lib/features/cousres/presentation/view/widgets/course_card.dart
@@ -16,7 +16,6 @@ class CourseCard extends StatelessWidget {
           GoRouter.of(context).push(AppRouter.kCourseDetailsView);
         },
         child: Container(
-          height: 120,
           decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.circular(10),
@@ -30,25 +29,39 @@ class CourseCard extends StatelessWidget {
             //   ),
             // ],
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Image.asset(AssetsData.logo, height: 50),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Advanced Robotics', style: Styles.textStyle18),
-                  Text(
-                    'Dr. Fatima Al-Mansour',
-                    style: Styles.textStyle16.copyWith(
-                      color: Colors.grey,
-                      fontWeight: FontWeight.w700,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 8, bottom: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  height: 120,
+                  width: 120,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.red,
+                    image: DecorationImage(
+                      image: AssetImage(AssetsData.course),
+                      fit: BoxFit.fill,
                     ),
                   ),
-                ],
-              ),
-              Icon(Icons.arrow_forward_ios_sharp, size: 22),
-            ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Advanced Robotics', style: Styles.textStyle18),
+                    Text(
+                      'Dr. Fatima Al-Mansour',
+                      style: Styles.textStyle16.copyWith(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+                Icon(Icons.arrow_forward_ios_sharp, size: 22),
+              ],
+            ),
           ),
         ),
       ),

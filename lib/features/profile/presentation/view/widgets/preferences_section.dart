94692f08c661f4d88b7f359d813d72951c16a71d
Ziad Tamer <ztamer494@gmail.com>
2026-04-28 19:36:12 +0300

    preference section done

--- lib/features/profile/presentation/view/widgets/preferences_section.dart
+++ lib/features/profile/presentation/view/widgets/preferences_section.dart
@@ -1,3 +1,4 @@
+import 'package:ai_eru_tawasol/core/utils/styles.dart';
 import 'package:ai_eru_tawasol/features/profile/presentation/view/widgets/custom_container_section.dart';
 import 'package:ai_eru_tawasol/features/profile/presentation/view/widgets/custom_list_tile.dart';
 import 'package:flutter/material.dart';
@@ -11,37 +12,65 @@ class PreferencesSection extends StatelessWidget {
       title: 'Preferences',
       children: [
         CustomListTile(
-          icon: Icons.person_outline_rounded,
-          title: 'User Name',
-          subtitle: 'Ziad Tamer',
-          trailing: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.arrow_forward_ios_rounded,
-              color: Color(0xff9E9D97),
-            ),
+          icon: Icons.brightness_5_outlined,
+          title: 'Theme Mode',
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Light',
+                style: Styles.textStyle16.copyWith(color: Color(0xff9E9D97)),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.arrow_forward_ios_rounded,
+                  color: Color(0xff9E9D97),
+                ),
+              ),
+            ],
           ),
         ),
         Divider(endIndent: 43, indent: 68.5, thickness: 0.5),
         CustomListTile(
-          icon: Icons.lock_outline_rounded,
-          title: 'User Name',
-          subtitle: '********',
-          trailing: IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.visibility, size: 28, color: Color(0xff9E9D97)),
+          icon: Icons.notifications_none_rounded,
+          title: 'Notifications',
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Muted',
+                style: Styles.textStyle16.copyWith(color: Color(0xff9E9D97)),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.arrow_forward_ios_rounded,
+                  color: Color(0xff9E9D97),
+                ),
+              ),
+            ],
           ),
         ),
         Divider(endIndent: 43, indent: 68.5, thickness: 0.5),
         CustomListTile(
-          icon: Icons.password_rounded,
-          title: 'Change Password',
-          trailing: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.arrow_forward_ios_rounded,
-              color: Color(0xff9E9D97),
-            ),
+          icon: Icons.language_rounded,
+          title: 'Language',
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'English',
+                style: Styles.textStyle16.copyWith(color: Color(0xff9E9D97)),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.arrow_forward_ios_rounded,
+                  color: Color(0xff9E9D97),
+                ),
+              ),
+            ],
           ),
         ),
       ],

a008cafec874a9dea4cedaf480722a4ef8063b10
Ziad Tamer <ztamer494@gmail.com>
2026-04-28 15:37:19 +0300

    add profile feature to BnB

--- lib/features/home/presentation/view/widgets/pages.dart
+++ lib/features/home/presentation/view/widgets/pages.dart
@@ -1,10 +1,11 @@
 import 'package:ai_eru_tawasol/features/announcements/presentation/view/announcements_view.dart';
 import 'package:ai_eru_tawasol/features/cousres/presentation/view/widgets/courses_view_body.dart';
+import 'package:ai_eru_tawasol/features/profile/presentation/view/profile_view.dart';
 import 'package:flutter/material.dart';
 
 final List<Widget> pages = const [
   AnnouncementsView(),
   CoursesViewBody(),
   Center(child: Text("Calendar")),
-  Center(child: Text("Profile")),
+  ProfileView(),
 ];

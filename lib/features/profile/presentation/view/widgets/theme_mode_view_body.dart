d1bb4ceebc03c4a42d494a3a0cc189d0cf2118ae
Ziad Tamer <ztamer494@gmail.com>
2026-04-29 15:20:48 +0300

    manage theme mode selection

--- lib/features/profile/presentation/view/widgets/theme_mode_view_body.dart
+++ lib/features/profile/presentation/view/widgets/theme_mode_view_body.dart
@@ -1,8 +1,16 @@
 import 'package:ai_eru_tawasol/features/profile/presentation/view/widgets/additional_pages_app_bar.dart';
+import 'package:ai_eru_tawasol/features/profile/presentation/view/widgets/theme_mode_options.dart';
 import 'package:flutter/material.dart';
 
-class ThemeModeViewBody extends StatelessWidget {
+class ThemeModeViewBody extends StatefulWidget {
   const ThemeModeViewBody({super.key});
+
+  @override
+  State<ThemeModeViewBody> createState() => _ThemeModeViewBodyState();
+}
+
+class _ThemeModeViewBodyState extends State<ThemeModeViewBody> {
+  ThemeModeOptions selectedMode = ThemeModeOptions.light;
 
   @override
   Widget build(BuildContext context) {

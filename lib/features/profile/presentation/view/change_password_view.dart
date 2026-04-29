2d1ba6c78c071565eff92c571660275b8ff7923d
Ziad Tamer <ztamer494@gmail.com>
2026-04-29 09:10:17 +0300

    add ChangePasswordViewBody to ChangePasswordView

---
+++ lib/features/profile/presentation/view/change_password_view.dart
@@ -0,0 +1,14 @@
+import 'package:ai_eru_tawasol/features/profile/presentation/view/widgets/change_password_view_body.dart';
+import 'package:flutter/material.dart';
+
+class ChangePasswordView extends StatelessWidget {
+  const ChangePasswordView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ChangePasswordViewBody(),
+    );
+  }
+}

6e3c7f79cc449dcae74ac196ca9598bd8df89773
Ziad Tamer <ztamer494@gmail.com>
2026-04-30 22:46:58 +0300

    app version ui part 1 done

--- lib/features/profile/presentation/view/widgets/app_version_view_body.dart
+++ lib/features/profile/presentation/view/widgets/app_version_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:ai_eru_tawasol/constants.dart';
+import 'package:ai_eru_tawasol/core/utils/styles.dart';
 import 'package:ai_eru_tawasol/features/profile/presentation/view/widgets/additional_pages_app_bar.dart';
 import 'package:ai_eru_tawasol/features/profile/presentation/view/widgets/custom_about_me_header.dart';
 import 'package:flutter/material.dart';
@@ -10,12 +12,76 @@ class AppVersionViewBody extends StatelessWidget {
     return Column(
       children: [
         AdditionalPagesAppBar(title: 'App Version'),
-        SizedBox(height: 20),
+        const SizedBox(height: 20),
         CustomAboutMeHeader(
           image: Placeholder(fallbackHeight: 200),
           title: 'App Version',
           subTitile: '1.0.0+1',
           paragraph: 'You are using the latest version of\nAI Connect.',
+        ),
+        const SizedBox(height: 20),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            height: 200,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white70,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  blurRadius: 10,
+                  spreadRadius: 2,
+                  offset: const Offset(0, 4),
+                ),
+              ],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("What's New?", style: Styles.textStyle24),
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    Text("• ", style: Styles.textStyle18),
+                    Text(
+                      " Performance improvements",
+                      style: Styles.textStyle18.copyWith(
+                        color: kSecondaryColor,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text("• ", style: Styles.textStyle18),
+                    Text(
+                      " Bug fixes",
+                      style: Styles.textStyle18.copyWith(
+                        color: kSecondaryColor,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text("• ", style: Styles.textStyle18),
+                    Text(
+                      " Better user experience",
+                      style: Styles.textStyle18.copyWith(
+                        color: kSecondaryColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
       ],
     );

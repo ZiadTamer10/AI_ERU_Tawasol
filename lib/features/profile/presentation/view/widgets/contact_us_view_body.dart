4fa14e5446bfb29550844047c3b2eb5f4a4257f1
Ziad Tamer <ztamer494@gmail.com>
2026-04-30 22:12:26 +0300

    contact us card ui done

--- lib/features/profile/presentation/view/widgets/contact_us_view_body.dart
+++ lib/features/profile/presentation/view/widgets/contact_us_view_body.dart
@@ -1,10 +1,69 @@
+import 'package:ai_eru_tawasol/constants.dart';
+import 'package:ai_eru_tawasol/core/utils/styles.dart';
+import 'package:ai_eru_tawasol/features/profile/presentation/view/widgets/additional_pages_app_bar.dart';
+import 'package:ai_eru_tawasol/features/profile/presentation/view/widgets/custom_container_section.dart';
+import 'package:ai_eru_tawasol/features/profile/presentation/view/widgets/custom_list_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ContactUsViewBody extends StatelessWidget {
   const ContactUsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        AdditionalPagesAppBar(title: 'Contact Us'),
+        SizedBox(height: 30),
+        Text(
+          "We'd love to hear from you",
+          style: Styles.textStyle18.copyWith(fontWeight: FontWeight.w600),
+        ),
+        SizedBox(height: 20),
+        CustomContainerSection(
+          children: [
+            CustomListTile(
+              icon: FaIcon(
+                FontAwesomeIcons.solidEnvelope,
+                color: kPrimaryColor,
+                size: 32,
+              ),
+              title: 'Email',
+              subtitle: 'support@aiconnect.com',
+            ),
+            const SizedBox(height: 10),
+            CustomListTile(
+              icon: FaIcon(
+                FontAwesomeIcons.phone,
+                color: kPrimaryColor,
+                size: 32,
+              ),
+              title: 'Phone',
+              subtitle: '+20 101111002',
+            ),
+            const SizedBox(height: 10),
+            CustomListTile(
+              icon: FaIcon(
+                FontAwesomeIcons.locationDot,
+                color: kPrimaryColor,
+                size: 32,
+              ),
+              title: 'Address',
+              subtitle: '123 Smart City, Cairo,Egypt',
+            ),
+            const SizedBox(height: 10),
+            CustomListTile(
+              icon: FaIcon(
+                FontAwesomeIcons.clock,
+                color: kPrimaryColor,
+                size: 32,
+              ),
+              title: 'Working Hours',
+              subtitle: 'Sat - Thu: 9:00 AM - 6:00 PM',
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }

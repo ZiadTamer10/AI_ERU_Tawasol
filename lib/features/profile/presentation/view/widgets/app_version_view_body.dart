2342edc84f9c12a1e6b3a4dd4ea73cc0f3a7e7d3
Ziad Tamer <ztamer494@gmail.com>
2026-04-30 22:29:48 +0300

    add CustomAboutMeHeader to app version view body

--- lib/features/profile/presentation/view/widgets/app_version_view_body.dart
+++ lib/features/profile/presentation/view/widgets/app_version_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:ai_eru_tawasol/features/profile/presentation/view/widgets/additional_pages_app_bar.dart';
+import 'package:ai_eru_tawasol/features/profile/presentation/view/widgets/custom_about_me_header.dart';
 import 'package:flutter/material.dart';
 
 class AppVersionViewBody extends StatelessWidget {
@@ -10,6 +11,12 @@ class AppVersionViewBody extends StatelessWidget {
       children: [
         AdditionalPagesAppBar(title: 'App Version'),
         SizedBox(height: 20),
+        CustomAboutMeHeader(
+          image: Placeholder(fallbackHeight: 200),
+          title: 'App Version',
+          subTitile: '1.0.0+1',
+          paragraph: 'You are using the latest version of\nAI Connect.',
+        ),
       ],
     );
   }

f5e38d2af7fb09fc43fad9509de4ccb845617b2f
Ziad Tamer <ztamer494@gmail.com>
2026-05-13 13:19:01 +0300

    remove unused import

--- lib/features/home/presentation/view/widgets/pages.dart
+++ lib/features/home/presentation/view/widgets/pages.dart
@@ -1,7 +1,6 @@
 import 'package:ai_eru_tawasol/features/announcement/data/models/announ_models.dart';
 import 'package:ai_eru_tawasol/features/announcement/presentation/view/announ_view.dart';
 import 'package:ai_eru_tawasol/features/chat/presentation/view/chat_view.dart';
-import 'package:ai_eru_tawasol/features/cousres/presentation/view/courses_view.dart';
 import 'package:ai_eru_tawasol/features/profile/presentation/view/profile_view.dart';
 import 'package:flutter/material.dart';
 
